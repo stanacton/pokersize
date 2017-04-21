98f10806a6b1358afb1dab1fe4f94792c1b58867
stanacton <stanacton@gmail.com>
2017-04-21 23:27:45 +0100

    removed angular

--- User.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class User {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-}
-exports.User = User;
